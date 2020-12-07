4282c74eaa15de0bc72a4abc1dfb2335f212324f
Santiago Quintero <quinterosantiago15@gmail.com>
2020-12-07 08:23:29 -0500

    Creating styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+// ?1.
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+// ?2.
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0F574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFFFFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 function App() {
+
+  // ?3.
+  const queryAPI = () => {
+    
+  }
+
   return (
-    <h1>Hola Mundo!</h1>
+    <Container>
+      <Button
+        onClick={queryAPI}
+      >
+        Obtener Frase
+      </Button>
+    </Container>
   );
 }
 
